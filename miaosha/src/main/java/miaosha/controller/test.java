cf1812155e6c4f073f3bface008a683b6da98a10
baila <737273791@qq.com>
2016-08-24 00:45:53 +0800

    添加jredis 添加测试类

--- miaosha/src/main/java/miaosha/controller/test.java
+++ miaosha/src/main/java/miaosha/controller/test.java
@@ -1,10 +1,23 @@
 package miaosha.controller;
 
+import redis.clients.jedis.Jedis;
+
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Jedis jedis = new Jedis("192.168.0.111");
+	    long start = System.currentTimeMillis();
+	    for (int i = 0; i < 100000; i++) {
+	        String result = jedis.set("test" + i, "n" + i);
+	    }
+	    long end = System.currentTimeMillis();
+	    System.out.println("Simple SET: " + ((end - start)/1000.0) + " seconds");
+		
+//		String name = jedis.get("baila");
+//		System.out.println(name);
+		
+	    jedis.disconnect();
 	}
 
 }
