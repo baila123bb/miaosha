4d50dd23856cae59db7e4ca425135b8d6177818d
baila <737273791@qq.com>
2016-08-28 15:53:46 +0800

    修复问题

--- miaosha/src/main/java/miaosha/controller/commons/chatController.java
+++ miaosha/src/main/java/miaosha/controller/commons/chatController.java
@@ -5,10 +5,15 @@ package miaosha.controller.commons;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import miaosha.controller.BaseController;
+import miaosha.manager.MemberManager;
+import miaosha.model.Member;
 
 /** 
  * 
@@ -20,11 +25,16 @@ import miaosha.controller.BaseController;
 @RequestMapping("/chat")
 public class chatController extends BaseController {
 	
+	@Autowired
+	private MemberManager memberManager;
+	
 	@RequestMapping("/to")
 	public String index(HttpServletRequest request, 
 						HttpServletResponse response,
 			  			Model model){
-	
+		Member m = memberManager.findMemById(1);
+		System.out.println(m.getUsername());
+		
 		return "commons/chat";
 	}
 	
